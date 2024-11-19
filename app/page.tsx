837a72d430b0af2ce0a1a7d748ac84b7d433de24
unknown <mugq678@gmail.com>
2024-11-19 23:15:31 +0500

    initial pro and setup sanaty.io

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import {groq} from "next-sanity"
+import { client } from "@/sanity/lib/client";
+
+export default async function Home() {
+
+  const produts = await client.fetch (groq `*[_type=="product"]`)
+  console.log (produts)
+
+  return (
+    <>
+    <h1 className="underline">Hello World !</h1>
+    
+    </>
+  );
+}
