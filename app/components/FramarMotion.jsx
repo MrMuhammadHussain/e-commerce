8257a49d332f3eabb9656ce89a342c6c19eab2ac
MrMuhammadHussain <mugq678@gmail.com>
2024-11-22 22:15:28 +0500

    Hero section Done

---
+++ app/components/FramarMotion.jsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion"
+
+ () => {
+
+
+    const variants = {
+        hidden: { x: 0, y: 70, opacity: 0.25 },
+        visible: { x: 0, y: -10, opacity: 1, transition: { delay: 0.05 } },
+
+        
+    }
+    return (
+        <div>
+    
+    
+            <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={variants}>
+            </motion.div>
+
+
+        </div>
+    )
+}
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,30 +1,57 @@
+"use client"
 import { Banner, HeroImage } from '@/public'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "framer-motion"
 
 const Hero = () => {
+
+  const variants1 = {
+    hidden: { x: 0, y: -5, opacity: 0.25 },
+    visible: { x: 0, y: 10, opacity: 1, transition: { delay: 0.1 } },
+
+  }
+  const variants2 = {
+    hidden: { x: 0, y: 40, opacity: 0.25 },
+    visible: { x: 0, y: -10, opacity: 1, transition: { delay: 0.2 } },
+  }
   return (
-    <div className='w-full h-[450px] flex items-center '>
-        <div className=' h-full md:max-w-[1024px] max-w-[600px] m-auto flex justify-center items-center'>
-            <div className='object-cover '>
-                <Image
-                src={Banner}
-                height="100"
-                width="1400"
-                alt='Banner'
-                />
-            </div>
-            <div className='absolute md:mt-[300px] mt-[200px] ' >
-                <Image
-                src={HeroImage}
-                height="100"
-                width="700"
-                alt='Hero-Image'
-                />
-            </div>
+    <div className=' hero-section '>
+      <div className='hero-container'>
+        <div className='object-cover '>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={variants1}
+          >
+            <Image
+              src={Banner}
+              height="100"
+              width="1400"
+              alt='Banner'
+            />
+          </motion.div>
         </div>
+        <div className='absolute md:mt-[300px] mt-[200px] ' >
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={variants2}
+          >
+            <Image
+              src={HeroImage}
+              height="100"
+              width="700"
+              alt='Hero-Image'
+            />
+          </motion.div>
+
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Hero
+
+
