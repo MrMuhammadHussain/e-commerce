5375edfb31fb277737731d28be96a01511f696d8
MrMuhammadHussain <mugq678@gmail.com>
2024-11-27 22:54:47 +0500

    Sanity.io Data geting Code

---
+++ app/components/Sanity.io.jsx
@@ -0,0 +1,18 @@
+//Get the Data from Sanity.io and Show at web UI.
+    
+//     const Products  = async () => {
+//         const products = await client.fetch (groq `*[_type=="product"]`)
+//         <div className="border border-black mt-6">
+//                 {products.map((product:any,index:number) =>(
+//                     <div className="bg-white pt-10 drop-shadow-md rounded-lg overflow-hidden" key={index}>
+//                     <Image
+//                         src={urlFor(product.image[0]).url()}
+//                         alt={product.slug}
+//                         width={160}
+//                         height={120}
+//                         className="object-cover mx-auto h-40"
+//                         />
+//                 </div>
+// ))
+// }
+//             </div>
