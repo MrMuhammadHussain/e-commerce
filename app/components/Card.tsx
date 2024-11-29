b9d7ed6ccad0130ae53a0bafcf5adbd44e2e3902
MrMuhammadHussain <mugq678@gmail.com>
2024-11-29 23:31:29 +0500

    Done

---
+++ app/components/Card.tsx
@@ -0,0 +1,29 @@
+import { urlFor } from "@/sanity/lib/image"
+import Link from "next/link";
+import Image from "next/image"
+
+const Card = ({product}:any) => {
+    return (
+        <Link href={`/product/${product.slug.current}`}>
+            <div className="bg-white pt-10 drop-shadow-md rounded-lg overflow-hidden ">
+
+                <Image
+                    src={urlFor(product.image[0]).url()}
+                    alt={product.slug}
+                    width={150}
+                    height={100}
+                    className="object-cover h-32 mx-auto m-2 "
+                />
+                <div className="text-center py-4 drop-shadow-sm">
+                    <h1 className="font-bold text-2xl text-gray-950">{product.name}</h1>
+                    <h1 className="text-xl text-gray-700 font-bold">$ {product.price}</h1>
+
+                </div>
+            </div>
+        </Link>
+
+
+    )
+}
+
+export default Card
