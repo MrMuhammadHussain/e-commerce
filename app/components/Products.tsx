9d9d115cd96f1897eb71dbb9923d3e835b063ef2
MrMuhammadHussain <mugq678@gmail.com>
2024-11-29 23:31:18 +0500

    Done

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -3,6 +3,8 @@ import {groq} from "next-sanity"
 import { client } from "@/sanity/lib/client";
 // import { urlForImage } from "@/sanity/lib/image";
 import { urlFor } from "@/sanity/lib/image"
+import Link from "next/link";
+import Card from "./Card";
 
 const Products  = async () => {
     const products = await client.fetch (groq `*[_type=="product"]`)
@@ -19,20 +21,9 @@ const Products  = async () => {
             </div>
 
 
-            <div className="border border-black mt-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 mt-6 ">
                 {products.map((product:any,index:number) =>(
-                    
-                    <div className="bg-white pt-10 drop-shadow-md rounded-lg overflow-hidden" key={index}>
-                        
-                    <Image
-                        src={urlFor(product.image[0]).url()}
-                        alt={product.slug}
-                        width={160}
-                        height={120}
-                        className="object-cover mx-auto h-40"
-                        />
-                </div>
-
+                <Card  product={product} key={index} />
 
                     
                 ))
